7307179a4f0ffd9859d9b63723f38fa72a55ef91
Allef Brendel <allef.brendel@dce.ufpb.br>
2019-06-30 19:15:22 -0300

    Adicionando FloatActionButton ao projeto

---
+++ app/src/main/java/com/example/macaxeiratec/MainActivity.java
@@ -0,0 +1,258 @@
+package com.example.macaxeiratec;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import com.example.macaxeiratec.domain.Personagens;
+import com.example.macaxeiratec.fragments.PersonagensFragments;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.interfaces.OnCheckedChangeListener;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
+import com.mikepenz.materialdrawer.model.SwitchDrawerItem;
+import com.mikepenz.materialdrawer.model.ToggleDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private Toolbar mToolbarBottom;
+    private Drawer navigationDrawerLeft;
+    private Drawer navigationDrawerRight;
+    private AccountHeader headerNavigationLeft;
+    private int mPositionClicked;
+
+    private OnCheckedChangeListener mOnCheckedChangeListener = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(IDrawerItem drawerItem, CompoundButton buttonView, boolean isChecked) {
+            Toast.makeText(MainActivity.this, "onCheckedChanged "+(isChecked ? "true" : "false"), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.tb_main);
+        mToolbar.setTitle("Konoha");
+        mToolbar.setSubtitle("Descrição dos ninjas");
+        mToolbar.setLogo(R.drawable.konoha_simbolo);
+        setSupportActionBar(mToolbar);
+
+      /*  mToolbarBottom = findViewById(R.id.inc_tb_bottom);
+        mToolbarBottom.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent it = null;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.action_facebook:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://www.facebook.com"));
+                        break;
+                    case R.id.action_youtube:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://www.youtube.com"));
+                        break;
+                    case R.id.action_google_plus:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://plus.google.com"));
+                        break;
+                    case R.id.action_linkedin:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://www.linkedin.com"));
+                        break;
+                    case R.id.action_whatsapp:
+                        it = new Intent(Intent.ACTION_VIEW);
+                        it.setData(Uri.parse("http://www.whatsapp.com"));
+                        break;
+                }
+                startActivity(it);
+                return true;
+            }
+        });
+        mToolbarBottom.inflateMenu(R.menu.menu_bottom);
+
+        mToolbarBottom.findViewById(R.id.iv_settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Botão Configurações Precionado", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        //  FRAGMENT
+        PersonagensFragments frag = (PersonagensFragments) getSupportFragmentManager().findFragmentByTag("person_frag");
+        if (frag == null) {
+            frag = new PersonagensFragments();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.rl_fragment_container, frag, "person_frag");
+            ft.commit();
+        }
+
+        //HEADER
+        headerNavigationLeft = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withCompactStyle(false)
+                .withSavedInstance(savedInstanceState)
+                //.withProfileImagesVisible(false)
+                .withThreeSmallProfileImages(true)
+                .withHeaderBackground(R.drawable.clan_uzumaki)
+                .addProfiles(
+                        new ProfileDrawerItem().withName("Kakashi").withEmail("kakashi@gmail.com").withIcon(getResources().getDrawable(R.drawable.kakashi_foto)),
+                        new ProfileDrawerItem().withName("Naruto").withEmail("naruto@gmail.com").withIcon(getResources().getDrawable(R.drawable.naruto_foto)),
+                        new ProfileDrawerItem().withName("Sakura").withEmail("sakura@gmail.com").withIcon(getResources().getDrawable(R.drawable.sakura_foto)),
+                        new ProfileDrawerItem().withName("Sasuke").withEmail("sasuke@gmail.com").withIcon(getResources().getDrawable(R.drawable.sasuke_foto))
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean current) {
+                        Toast.makeText(MainActivity.this, "onProfileChanged: "+profile.getName(), Toast.LENGTH_SHORT).show();
+                        headerNavigationLeft.setBackgroundRes(R.drawable.clan_uchiha);
+                        return false;
+                    }
+                })
+                .build();
+
+        //NAVIGATION DRAWER
+        navigationDrawerLeft = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .withSliderBackgroundColor(getResources().getColor(R.color.colorCardViwe))
+                .withDisplayBelowStatusBar(true)
+                .withActionBarDrawerToggleAnimated(true)
+                .withDrawerGravity(Gravity.LEFT)
+                .withSavedInstance(savedInstanceState)
+                .withSelectedItem(0)
+                .withAccountHeader(headerNavigationLeft)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                       /*for(int cont = 0, tam = navigationDrawerLeft.getDrawerItems().size(); cont < tam; cont++){
+                            if (cont == mPositionClicked && mPositionClicked <= 3 ){
+                                PrimaryDrawerItem aux = (PrimaryDrawerItem) navigationDrawerLeft.getDrawerItems().get(cont);
+                                aux.withIcon(getResources().getDrawable(getCorrectDrawerIcon(cont,false)));
+                                break;
+                            }
+                        }
+                        if(position <= 3){
+                            ((PrimaryDrawerItem)drawerItem).withIcon(getResources().getDrawable(getCorrectDrawerIcon(position,true)));
+                        }
+                            mPositionClicked = position;
+                            navigationDrawerLeft.getAdapter().notifyAdapterDataSetChanged();*/
+                        return false;
+                    }
+                })
+                .withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(View view, int position, IDrawerItem drawerItem) {
+                        Toast.makeText(MainActivity.this, "onItemLongClick: " + position, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                })
+                .build();
+
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem().withName("Vila da Folha").withIcon(getResources().getDrawable(R.drawable.car_selected_1)));
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem().withName("Vila da Areia").withIcon(getResources().getDrawable(R.drawable.car_selected_2)));
+        navigationDrawerLeft.addItem(new DividerDrawerItem());
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem().withName("Vila da Névoa").withIcon(getResources().getDrawable(R.drawable.car_selected_3)));
+        navigationDrawerLeft.addItem(new PrimaryDrawerItem().withName("Vila da Chuva").withIcon(getResources().getDrawable(R.drawable.car_selected_4)));
+        navigationDrawerLeft.addItem(new SectionDrawerItem().withName("Configurações").withTextColor((getResources().getColor(R.color.colorPrimarytext))));
+        navigationDrawerLeft.addItem(new SwitchDrawerItem().withName("Notificação").withChecked(true).withOnCheckedChangeListener(mOnCheckedChangeListener));
+        navigationDrawerLeft.addItem(new ToggleDrawerItem().withName("News").withChecked(true).withOnCheckedChangeListener(mOnCheckedChangeListener));
+
+        //END - RIGHT
+        /*navigationDrawerRight = new DrawerBuilder()
+                .withActivity(this)
+                //.withToolbar(mToolbar)
+                .withDisplayBelowStatusBar(false)
+                .withActionBarDrawerToggleAnimated(true)
+                .withDrawerGravity(Gravity.END)
+                .withSavedInstance(savedInstanceState)
+                .withSelectedItem(-1)
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        Toast.makeText(MainActivity.this, "onItemClick: " + position, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                })
+                .withOnDrawerItemLongClickListener(new Drawer.OnDrawerItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(View view, int position, IDrawerItem drawerItem) {
+                        Toast.makeText(MainActivity.this, "onItemLongClick: " + position, Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                })
+                .build();*/
+
+    }
+
+    private int getCorrectDrawerIcon(int position, boolean isSelected){
+        switch (position){
+            case 0:
+                return ( isSelected ? R.drawable.car_selected_1 : R.drawable.car_1 );
+            case 1:
+                return ( isSelected ? R.drawable.car_selected_2 : R.drawable.car_2 );
+            case 2:
+                return ( isSelected ? R.drawable.car_selected_3 : R.drawable.car_3 );
+            case 3:
+                return ( isSelected ? R.drawable.car_selected_4 : R.drawable.car_4 );
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_second_activity){
+            startActivity(new Intent(this, SecondActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public List<Personagens> getSetPersonagensList(int quant){
+        String[] nome = new String[]{"Naruto Uzumaki","Sasuke Uchiha","Sakura Haruno ","Kakashi Hatake","Shikamaru Nara","Sai","Ino Yamanaka","Maito Gai","Itachi Uchiha","Orochimaru","Rock Lee"};
+        String[] clan = new String[]{"Uzumaki","Uchiha","Uchiha","Hatake","Nara","Yamanaka","Yamanaka","Desconhecido","Uchiha","Desconhecido","Lee"};
+        int[] fotos = new int[]{R.drawable.naruto_foto,R.drawable.sasuke_foto,R.drawable.sakura_foto,R.drawable.kakashi_foto,R.drawable.shikamaru_foto,R.drawable.sai_foto,R.drawable.ino_foto,R.drawable.gai_foto,R.drawable.itachi_foto,R.drawable.orochimaru_foto,R.drawable.lee_foto};
+        List<Personagens> listAux = new ArrayList<>();
+
+        for (int i = 0; i < quant; i++){
+            Personagens personagens = new Personagens(nome[i % nome.length], clan[i % clan.length], fotos[i % nome.length]);
+            listAux.add(personagens);
+        }
+        return listAux;
+
+    }
+
+}
+
--- app/src/main/java/com/example/macaxeiratec/fragments/PersonagensFragments.java
+++ app/src/main/java/com/example/macaxeiratec/fragments/PersonagensFragments.java
@@ -1,14 +1,14 @@
 package com.example.macaxeiratec.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -21,15 +21,19 @@ import com.daimajia.androidanimations.library.YoYo;
 import com.example.macaxeiratec.R;
 import com.example.macaxeiratec.adapters.Personagens_Adapter;
 import com.example.macaxeiratec.domain.Personagens;
-import com.example.macaxeiratec.interfaces.MainActivity;
+import com.example.macaxeiratec.MainActivity;
 import com.example.macaxeiratec.interfaces.RecycleViewOnClickListinerHack;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 import java.util.List;
 
-public class PersonagensFragments extends Fragment implements RecycleViewOnClickListinerHack {
+public class PersonagensFragments extends Fragment implements RecycleViewOnClickListinerHack, View.OnClickListener {
 
-    private RecyclerView mRecyclerView;
-    private List<Personagens> mList;
+    public RecyclerView mRecyclerView;
+    public List<Personagens> mList;
+    //private FloatingActionButton fab;
+    public FloatingActionMenu fab;
 
     @Nullable
     @Override
@@ -48,8 +52,14 @@ public class PersonagensFragments extends Fragment implements RecycleViewOnClick
             public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
 
+                if(dy > 0){
+                    fab.hideMenuButton(true);
+                }
+                else{
+                    fab.showMenuButton(true);
+                }
 
-               LinearLayoutManager llm = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+                LinearLayoutManager llm = (LinearLayoutManager) mRecyclerView.getLayoutManager();
                 //GridLayoutManager llm = (GridLayoutManager) mRecyclerView.getLayoutManager();
 
                /* StaggeredGridLayoutManager llm = (StaggeredGridLayoutManager) mRecyclerView.getLayoutManager();
@@ -91,9 +101,78 @@ public class PersonagensFragments extends Fragment implements RecycleViewOnClick
         Personagens_Adapter adapter = new Personagens_Adapter(getActivity(),mList);
       //  adapter.setRecycleViewOnClickListinerHack(this);
         mRecyclerView.setAdapter(adapter);
+
+        fab = getActivity().findViewById(R.id.fab);
+        fab.setOnMenuToggleListener(new FloatingActionMenu.OnMenuToggleListener() {
+            @Override
+            public void onMenuToggle(boolean opened) {
+                Toast.makeText(getActivity(), "Abriu? " + (opened ? "true" : "false"), Toast.LENGTH_SHORT).show();
+            }
+        });
+        fab.showMenuButton(true);
+        fab.setClosedOnTouchOutside(true);
+
+        FloatingActionButton fab1 = getActivity().findViewById(R.id.fab1);
+        FloatingActionButton fab2 = getActivity().findViewById(R.id.fab2);
+        FloatingActionButton fab3 = getActivity().findViewById(R.id.fab3);
+        FloatingActionButton fab4 = getActivity().findViewById(R.id.fab4);
+        FloatingActionButton fab5 = getActivity().findViewById(R.id.fab5);
+
+        fab1.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+        fab3.setOnClickListener(this);
+        fab4.setOnClickListener(this);
+        fab5.setOnClickListener(this);
+
+        /*fab = getActivity().findViewById(R.id.fab);
+        fab.attachToRecyclerView(mRecyclerView, new ScrollDirectionListener() {
+            @Override
+            public void onScrollDown() {
+
+            }
+
+            @Override
+            public void onScrollUp() {
+
+            }
+        },new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+
+                LinearLayoutManager llm = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+                //GridLayoutManager llm = (GridLayoutManager) mRecyclerView.getLayoutManager();
+
+               /* StaggeredGridLayoutManager llm = (StaggeredGridLayoutManager) mRecyclerView.getLayoutManager();
+                int[] aux = llm.findLastCompletelyVisibleItemPositions(null);
+                int max = -1;
+                for(int i = 0; i < aux.length;i++){
+                    max = aux[i] > max ? aux[i] : max;
+                }*/
+
+                /*Personagens_Adapter adapter = (Personagens_Adapter) mRecyclerView.getAdapter();
+
+
+                if(mList.size() == llm.findLastCompletelyVisibleItemPosition() + 1){
+                    //if(mList.size() == max + 1){
+                    List<Personagens> listAux = ((MainActivity) getActivity()).getSetPersonagensList(11);
+
+                    for(int i = 0; i < listAux.size(); i ++){
+                        adapter.addListItem(listAux.get(i), mList.size());
+                    }
+                }
+            }
+        });
+       fab.setOnClickListener(this);*/
+
         return view;
     }
-
     @Override
     public void onClickListiner(View view, int position) {
        // Toast.makeText(getActivity(), "onClickListiner: "+position, Toast.LENGTH_SHORT).show();
@@ -125,6 +204,35 @@ public class PersonagensFragments extends Fragment implements RecycleViewOnClick
 
         /*Personagens_Adapter adapter = (Personagens_Adapter) mRecyclerView.getAdapter();
         adapter.removeListItem(position);*/
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent it = null;
+
+        switch (v.getId()){
+            case R.id.fab1:
+                it = new Intent(Intent.ACTION_VIEW);
+                it.setData(Uri.parse("http://www.facebook.com"));
+                break;
+            case R.id.fab2:
+                it = new Intent(Intent.ACTION_VIEW);
+                it.setData(Uri.parse("http://plus.google.com"));
+                break;
+            case R.id.fab3:
+                it = new Intent(Intent.ACTION_VIEW);
+                it.setData(Uri.parse("http://www.whatsapp.com"));
+                break;
+            case R.id.fab4:
+                it = new Intent(Intent.ACTION_VIEW);
+                it.setData(Uri.parse("http://www.linkedin.com"));
+                break;
+            case R.id.fab5:
+                it = new Intent(Intent.ACTION_VIEW);
+                it.setData(Uri.parse("http://www.youtube.com"));
+                break;
+        }
+        startActivity(it);
     }
 
     private static class RecyclerViewTouchListener implements RecyclerView.OnItemTouchListener{
